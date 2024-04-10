1685b5b036da417fc6953528c5613aebcf0b1d7c
Kahe Mikáyas <132231345+Kahe-Mikaya@users.noreply.github.com>
2024-04-10 13:35:56 +0000

    criando o h1 titulo da loja e adicionando o seu css

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ let cookiesPorClick = 1;
 let cookiesPorSegundo = 0;
 let cookie = document.querySelector("#cookie");
 
-let quantidade = document.querySelector("#quantidade");
 
-cookie.addEventListener("click",function(){
+let quantidade = document.querySelector("#quantidade");
+let elementoRotativo = document.querySelector(".rotacoes");
+cookie.addEventListener("click",function(event){
   totalCookies += cookiesPorClick;
   quantidade.textContent = totalCookies;
+
+ 
+  
 })
