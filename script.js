1203add3194d421792c137f4a672100d9b713587
Kahe Mikáyas <132231345+Kahe-Mikaya@users.noreply.github.com>
2024-04-15 12:36:47 +0000

    fazendo o primeiro personagem funcionar

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+class Personagem  {
+
+   constructor(nome, cps, custo, quantidade){
+    this.nome = nome;
+    this.cps = cps;
+    this.custo = custo;
+    this.quantidade = quantidade;
+   }
+
+}
+
+async function clicar(personagem){
+  totalCookies += personagem.cps*personagem.quantidade;
+  console.log(totalCookies);
+  quantidadeCookies.textContent = parseInt(totalCookies);
+  setTimeout(() => clicar(personagem), 1000);
+
+
+}
+
+let cursor_personagem = new Personagem("Cursor", 0.1, 15, 0);
+
 let totalCookies = 0;
 let cookiesPorClick = 1;  
 
@@ -7,13 +29,19 @@ let cookie = document.querySelector("#cookie");
 let cursor = document.querySelector("#cursor");
 
 
-let quantidade = document.querySelector("#quantidade");
+let quantidadeCookies = document.querySelector("#quantidade");
 let elementoRotativo = document.querySelector(".rotacoes");
 cookie.addEventListener("click",function(event){
   totalCookies += cookiesPorClick;
-  quantidade.textContent = totalCookies;
-  
+  quantidadeCookies.textContent = totalCookies;
+})
 
- 
+cursor.addEventListener("click",function(){
+  if (totalCookies >= cursor_personagem.custo){
+    totalCookies -= cursor_personagem.custo;
+    cursor_personagem.quantidade += 1;
+    if (cursor_personagem.quantidade == 1){
+      clicar(cursor_personagem);}
+  }  
   
 })
