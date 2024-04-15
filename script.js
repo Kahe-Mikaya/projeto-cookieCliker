97eed4d84343f3f313d8338c225937a7cc76445b
Kahe Mikáyas <132231345+Kahe-Mikaya@users.noreply.github.com>
2024-04-15 16:06:15 +0000

    adicionando json,  modulando os botoes, fazendo funcionar as funçoes assincronas e adicionando alguns personagens

--- script.js
+++ script.js
@@ -1,24 +1,63 @@
-class Personagem  {
+let menu_personagens = document.querySelector('.menu-personagens');
+function adicionarPersonagem(personagem){
+  let botao_adicionar = document.createElement("button");
+  botao_adicionar.classList.add("personagens");
+  menu_personagens.appendChild(botao_adicionar);
+  let personagem_imagem = document.createElement("p")
+  personagem_imagem.classList.add("personagem_imagem");
+  personagem_imagem.style.backgroundImage = `url(${personagem.imagem})`;
+  botao_adicionar.appendChild(personagem_imagem);
+  let titulo_personagem = document.createElement("div");
+  titulo_personagem.classList.add("titulo-personagem");
+  let nome_personagem = document.createElement("h2");
+  nome_personagem.classList.add("nome-personagem");
+  nome_personagem.textContent = personagem.nome;
+  titulo_personagem.appendChild(nome_personagem);
+  let custo_personagem = document.createElement("p");
+  custo_personagem.classList.add("custo-personagem");
+  custo_personagem.textContent = `Custo: ${personagem.preco}`;
+  titulo_personagem.appendChild(custo_personagem);
+  async function clicar(personagem){
+    totalCookies += personagem.cps*personagem.quantidade;
+    console.log(totalCookies);
+    quantidadeCookies.textContent = parseInt(totalCookies);
+    setTimeout(() => clicar(personagem), 1000);
+  }
 
-   constructor(nome, cps, custo, quantidade){
-    this.nome = nome;
-    this.cps = cps;
-    this.custo = custo;
-    this.quantidade = quantidade;
-   }
-
+  botao_adicionar.appendChild(titulo_personagem);
+  let quantidade_personagem = document.createElement("p");
+  quantidade_personagem.classList.add("quantidade-personagem");
+  quantidade_personagem.textContent = personagem.quantidade;
+  botao_adicionar.appendChild(quantidade_personagem);
+  botao_adicionar.addEventListener("click",function(){
+    console.log(personagem.nome);
+    if (totalCookies >= personagem.preco){
+      totalCookies -= personagem.preco;
+      personagem.quantidade += 1;
+      if (personagem.quantidade == 1){
+        console
+        clicar(personagem);}
+    }  
+    
+  })
+  
 }
 
-async function clicar(personagem){
-  totalCookies += personagem.cps*personagem.quantidade;
-  console.log(totalCookies);
-  quantidadeCookies.textContent = parseInt(totalCookies);
-  setTimeout(() => clicar(personagem), 1000);
+var request = new XMLHttpRequest();
+request.open('GET', 'personagem.json');
+request.responseType = "json";
+request.send();
+request.onload = function () {
+  let perso = request.response;
+  console.log(perso);
+  for(i = 0; i< 4; i++){
+    console.log("entrou no for")
+    console.log(perso.personagens[i]);
+    adicionarPersonagem(perso.personagens[i]);
+  }
+  
+};
 
-
-}
-
-let cursor_personagem = new Personagem("Cursor", 0.1, 15, 0);
 
 let totalCookies = 0;
 let cookiesPorClick = 1;  
@@ -26,22 +65,12 @@ let cookiesPorClick = 1;
 let cookiesPorSegundo = 0;
 let cookie = document.querySelector("#cookie");
 
-let cursor = document.querySelector("#cursor");
+
 
 
 let quantidadeCookies = document.querySelector("#quantidade");
 let elementoRotativo = document.querySelector(".rotacoes");
 cookie.addEventListener("click",function(event){
   totalCookies += cookiesPorClick;
-  quantidadeCookies.textContent = totalCookies;
-})
-
-cursor.addEventListener("click",function(){
-  if (totalCookies >= cursor_personagem.custo){
-    totalCookies -= cursor_personagem.custo;
-    cursor_personagem.quantidade += 1;
-    if (cursor_personagem.quantidade == 1){
-      clicar(cursor_personagem);}
-  }  
-  
+  quantidadeCookies.textContent = parseInt(totalCookies);
 })
